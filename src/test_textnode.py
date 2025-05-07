80dc0d07bcebcd45fa8236c5f6bb1d5e8483452e
marekprudil <marek.prudil@gmail.com>
2025-05-07 16:21:48 +0200

    text to html node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -24,10 +24,22 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a link node", TextType.LINK)
         self.assertNotEqual(node, node2)
 
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
+    def test_text_invalid_type(self):
+        node = TextNode("asdf", None)      
+        with self.assertRaises(Exception):
+            html_node = text_node_to_html_node(node)
 
-
-
+    def test_text_to_html_link(self):
+        node = TextNode("This is a text node", TextType.LINK, "foo.bar")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.to_html(), '<a href="foo.bar">This is a text node</a>')
 
 if __name__ == "__main__":
     unittest.main()
